f6ef452c04784e1c73d7200c80590561ec47c6ba
Ahmed Zena <87375061+AhmedZena@users.noreply.github.com>
2024-02-01 22:42:21 +0200

    not found page

--- src/app/component/not-found/not-found.component.ts
+++ src/app/component/not-found/not-found.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
   imports: [],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  styleUrl: './not-found.component.scss',
 })
 export class NotFoundComponent {
+  constructor(private router: Router) {}
 
+  goBack() {
+    this.router.navigate(['/']);
+  }
 }
